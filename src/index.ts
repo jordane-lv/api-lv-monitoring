c365f408f7b16bcfe693ce5107da80b41254d292
jordane-lv <jordane.chaves@lvnetwork.com.br>
2022-06-27 09:24:40 -0300

    Fix Host Name Regex

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import 'dotenv/config';
 import 'reflect-metadata';
+import 'dotenv/config';
+
 import express from 'express';
 
 import './shared/container';
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -2,8 +2,14 @@ import { isIP } from 'net';
 
 export default {
   validateHostName: (hostName: string): boolean => {
-    const reHostName =
-      /^\d{5}[\s_][A-Z]{3}(?:[\s_]?-[\s_]?[A-Z]{3})?[\s_]-[\s_][A-Z-_\s]+\b$/gm;
+    const reCode = '\\d{5}';
+    const reInitials = '[A-Z]{3}(?:\\s?-\\s?[A-Z]{3})?';
+    const reName = '[A-Z0-9\\s-]+';
+
+    const reHostName = new RegExp(
+      `\\b${reCode}\\s${reInitials}\\s-\\s${reName}\\b`,
+      'g',
+    );
 
     return reHostName.test(hostName);
   },
