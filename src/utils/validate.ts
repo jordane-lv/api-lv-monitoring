7fde08319e4f1b28eee63995bfe0686a2662b698
jordane-chaves <jordane_chaves@yahoo.com.br>
2022-07-04 09:00:12 -0300

    Refactoring patterns

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,23 +1,25 @@
 import { isIP } from 'net';
 
-const reCode = '\\d{5}';
-const reInitials = '[A-Z]{3}(?:\\s?-\\s?[A-Z]{3})?';
-const reName = '[A-Z0-9\\s-]+';
+const patterns = {
+  code: '\\d{5}',
+  initials: '[A-Z]{3}(?:\\s?-\\s?[A-Z]{3})?',
+  name: '[A-Z0-9\\s-]+',
+};
 
 export default {
   validIpAddress: (ipAddress: string): boolean => {
     return isIP(ipAddress) !== 0;
   },
   validateCode: (code: string): boolean => {
-    const re = new RegExp(`^${reCode}$`, 'gm');
+    const re = new RegExp(`^${patterns.code}$`, 'gm');
     return re.test(code);
   },
   validateInitial: (initial: string): boolean => {
-    const re = new RegExp(`^${reInitials}$`, 'gm');
+    const re = new RegExp(`^${patterns.initials}$`, 'gm');
     return re.test(initial);
   },
   validHostName: (hostName: string): boolean => {
-    const re = new RegExp(`^${reName}$`, 'gm');
+    const re = new RegExp(`^${patterns.name}$`, 'gm');
     return re.test(hostName);
   },
 };
